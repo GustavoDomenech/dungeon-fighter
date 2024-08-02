074e0eb1fce62f307dce753e8d06cb2b32f4d37f
guiganassini17@gmail.com <guiganassini17@gmail.com>
2024-08-01 22:42:44 -0300

    esqueci kkkk

---
+++ Settings.java
@@ -0,0 +1,11 @@
+public class Settings {
+	/* Board settings */
+	public static final BOARD_ROWS = 5;
+	public static final BOARD_COLUMNS = 10;
+	/* Hero settings */
+	public static final X = 5;
+	public static final Y = 10;
+	/* Monster settings */
+	/* Trap settings */
+	public static final W = 10;
+	}
